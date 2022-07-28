569fc89aae13e676d4bdd3acb2c755b71625ae41
Gulf <zhaoguofeng93@gmail.com>
2022-07-29 00:43:06 +0800

    startup leetcode hot 100

---
+++ hot_100/001_两数之和.py
@@ -0,0 +1,24 @@
+# -*- coding: UTF-8 -*-
+"""
+题目链接: https://leetcode.cn/problems/two-sum/
+解题思路: num1+num2=target,核心是要要num2=target-num1是否在list中，通过字典来模拟哈希查找（时间复杂度为O(1)）
+字典描述：{元素值：元素下标}，如{2：1}
+"""
+from typing import List
+
+
+# 主代码
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        ha = dict()
+        for i in range(len(nums)):
+            if target-nums[i] in ha:
+                return [ha[target-nums[i]], i]
+            ha[nums[i]] = i
+
+
+# 测试用例
+if __name__ == "__main__":
+    s = Solution()
+    res = s.twoSum([2, 7, 11, 15], 18)
+    print(res)

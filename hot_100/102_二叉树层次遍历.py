bf8341b96b14d1eb01592dcbb7cc8b5f530d9e8b
gulf <gulf@gmail.com>
2022-08-17 23:48:56 +0800

    record_4

---
+++ hot_100/102_二叉树层次遍历.py
@@ -0,0 +1,51 @@
+# -*- coding: UTF-8 -*-
+"""
+题目链接: https://leetcode.cn/problems/binary-tree-level-order-traversal/
+解题思路: 层次遍历，逐层遍历，每层节点保存为子数组。利用队列。参考：https://juejin.cn/post/6844903807759941646
+"""
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+
+    def test_case(self):
+        num_4 = TreeNode(4, None, None)
+        num_5 = TreeNode(5, None, None)
+        num_6 = TreeNode(6, None, None)
+        num_7 = TreeNode(7, None, None)
+        num_3 = TreeNode(3, num_6, num_7)
+        num_2 = TreeNode(2, num_4, num_5)
+        root = TreeNode(1, num_2, num_3)
+        return root
+
+    def levelOrder(self, root: [TreeNode]) -> List[List[int]]:
+        if not root: return []
+        res = []
+        queue = [root]
+        while queue:
+            tmp = []
+            nex = []
+            for node in queue:
+                tmp.append(node.val)
+                if node.left:
+                    nex.append(node.left)
+                if node.right:
+                    nex.append(node.right)
+            queue = nex
+            res.append(tmp)
+        return res
+
+
+# 测试用例
+if __name__ == "__main__":
+    s = Solution()
+    root = s.test_case()
+    res = s.levelOrder(root)
+    print(res)
---
+++ hot_100/33_搜索旋转排序数组查找.py
@@ -0,0 +1,38 @@
+# -*- coding: UTF-8 -*-
+"""
+题目链接: https://leetcode.cn/problems/search-in-rotated-sorted-array/
+解题思路:有序数组经过旋转后将数组一分为二，其中一定有一个是有序的，另一个可能是有序，也能是部分有序。有序数组搜索-->二分查找
+"""
+from typing import List
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if len(nums) == 0: return -1
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (right + left) >> 1
+            if nums[mid] == target:
+                return mid
+            if nums[mid] > nums[left]:     # 落在数值较大的区间
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            elif nums[mid] < nums[right]:  # 落在数值较小的区间
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            else:                         # 当数组长度为2时，会出现这种情况
+                if nums[mid] == nums[right]:
+                    right -= 1
+                if nums[mid] == nums[left]:
+                    left += 1
+        return -1
+
+
+# 测试用例
+if __name__ == "__main__":
+    s = Solution()
+    test = [4, 5, 6, 7, 0, 1, 2]
+    res = s.search(test, 6)
+    print(res)

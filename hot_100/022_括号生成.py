5ab502ad07151545a1863b5d121031b2bafa0b6f
gulf <gulf@gmail.com>
2022-09-07 22:24:43 +0800

    record_11

---
+++ hot_100/022_括号生成.py
@@ -0,0 +1,40 @@
+# -*- coding: UTF-8 -*-
+"""
+题目链接: https://leetcode.cn/problems/generate-parentheses/?favorite=2cktkvj
+解题思路: 回溯问题，通过构造一颗隐式的树来求解，一般用深度优先算法。基本过程：
+https://leetcode.cn/problems/generate-parentheses/solution/hui-su-suan-fa-by-liweiwei1419/
+"""
+from typing import List
+
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+
+        res = []
+        cur_str = ''
+
+        def dfs(cur_str, left, right, n):
+            """
+            :param cur_str: 从根结点到叶子结点的路径字符串
+            :param left: 左括号还可以使用的个数
+            :param right: 右括号还可以使用的个数
+            :return:
+            """
+            if left == n and right == n:
+                res.append(cur_str)
+                return
+            if right < left:
+                return
+            if left < n:
+                dfs(cur_str + '(', left + 1, right, n)
+            if right < n:
+                dfs(cur_str + ')', left, right + 1, n)
+
+        dfs(cur_str, 0, 0, n)
+        return res
+
+
+if __name__ == "__main__":
+    s = Solution()
+    res = s.generateParenthesis(0)
+    print(res)
---
+++ hot_100/042_接雨水.py
@@ -0,0 +1,34 @@
+# -*- coding: UTF-8 -*-
+"""
+题目链接: https://leetcode.cn/problems/trapping-rain-water/?favorite=2cktkvj
+解题思路: 积水量取决于两侧墙的高度，故需要找到所有可以匹配的墙的组合，min(左墙高度，右墙高度）* （右侧墙下标-左侧墙下标-1），可以利用单调栈。
+        入栈原则：当前高度小于等于栈顶高度或栈空，入栈，指针后移。出栈原则：当前高度大于栈顶高度，出栈，计算出当前墙和栈顶的墙之间水的多少，
+        然后计算当前的高度和新栈的高度的关系，重复直到当前墙的高度不大于栈顶高度或者栈空，然后把当前墙入栈，指针后移。
+"""
+from typing import List
+
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        stack = list()
+        ans = 0
+        p = 0
+        while p < len(height):
+            while stack and height[stack[-1]] < height[p]:   # 出栈：当前指针对应当墙高度大于栈顶且栈不为空
+                pop_index = height[stack[-1]]   # 记录栈顶高度，
+                stack.pop()       # 出栈
+                if not stack:     # 栈空跳出将当前指针入栈
+                    break
+                distance = p - stack[-1] - 1      # 计算两墙之间的距离
+                h = min(height[p], height[stack[-1]])      # 木桶效应，取最小值
+                ans = ans + distance * (h - pop_index)     # 墙与'地面'的高度差与墙距相乘得出盛水量
+            stack.append(p)     # 入栈
+            p += 1              # 指针后移
+        return ans
+
+
+if __name__ == "__main__":
+    s = Solution()
+    test_data = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+    res = s.trap(test_data)
+    print(res)
